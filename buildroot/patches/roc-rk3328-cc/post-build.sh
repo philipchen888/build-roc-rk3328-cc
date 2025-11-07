748e11e042e389bc114132a399ff6f15e19e5bdc
philipchen888 <philipchen888@users.noreply.github.com>
2025-11-06 19:29:57 -0800

    update buildroot

--- buildroot/patches/roc-rk3328-cc/post-build.sh
+++ buildroot/patches/roc-rk3328-cc/post-build.sh
@@ -2,10 +2,11 @@
 
 BOARD_DIR="$(dirname $0)"
 
-install -m 0644 -D $BOARD_DIR/../../../../kernel/kernel/arch/arm64/boot/Image $BINARIES_DIR/Image
-install -m 0644 -D $BOARD_DIR/../../../../kernel/kernel/arch/arm64/boot/dts/rockchip/rk3328-roc-cc.dtb $BINARIES_DIR/rk3328-roc-cc.dtb
-install -m 0644 -D $BOARD_DIR/../../../../kernel/rk3328.conf $BINARIES_DIR/extlinux/extlinux.conf
+install -m 0644 -D $BOARD_DIR/../../../../../linux/linux/arch/arm64/boot/Image $BINARIES_DIR/Image
+install -m 0644 -D $BOARD_DIR/../../../../../linux/linux/arch/arm64/boot/dts/rockchip/rk3328-roc-cc.dtb $BINARIES_DIR/rk3328-roc-cc.dtb
+install -m 0644 -D $BOARD_DIR/../../../../../linux/rk3328.conf $BINARIES_DIR/extlinux/extlinux.conf
 
-install -m 0644 -D $BOARD_DIR/../../../../uboot/out/trust.img $BINARIES_DIR/trust.img
-install -m 0644 -D $BOARD_DIR/../../../../uboot/out/uboot.img $BINARIES_DIR/uboot.img
-install -m 0644 -D $BOARD_DIR/../../../../uboot/out/idbloader.img $BINARIES_DIR/idbloader.img
+# install -m 0644 -D "${BOARD_DIR}/extlinux.conf" "${TARGET_DIR}/boot/extlinux/extlinux.conf"
+install -m 0644 -D $BOARD_DIR/../../../../../uboot/out/trust.img $BINARIES_DIR/trust.img
+install -m 0644 -D $BOARD_DIR/../../../../../uboot/out/uboot.img $BINARIES_DIR/uboot.img
+install -m 0644 -D $BOARD_DIR/../../../../../uboot/out/idbloader.img $BINARIES_DIR/idbloader.img
